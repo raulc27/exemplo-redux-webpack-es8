c31ae584e5765f361f616772d3447070071e103f
Raul Castro <raulc27@gmail.com>
2020-04-29 13:45:55 -0300

    adicionando rotinas de login

--- src/index.js
+++ src/index.js
@@ -3,7 +3,23 @@ import {configureStore} from "@reduxjs/toolkit";
 const storeEnhancers = windows.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 
+const LOGIN_SUCCESS = "LOGIN_SUCCESS";
+const FETCH_LINKS_REQUEST = "FETCH_LINKS_REQUEST";
+const FETCH_LINKS_SUCESS = "FETCH_LINKS_SUCCESS";
 
+function loginSuccess(payload){
+  return {type:LOGIN_SUCCESS,payload};
+}
+
+function fetchLinksRequest(){
+  return {type:FETCH_LINKS_REQUEST};
+}
+
+function fetchLinksSuccess(payload){
+  return {type: FETCH_LINKS_SUCCESS, payload};
+}
+ 
+// ********************************************************
 
 const middleware=[
 
