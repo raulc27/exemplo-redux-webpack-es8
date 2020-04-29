55f3d8466f0bd5f5ae38b93885180aa756fe6898
Raul Castro <raulc27@gmail.com>
2020-04-29 13:22:54 -0300

    adicionando auth

--- src/index.js
+++ src/index.js
@@ -1,17 +1,25 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import {createStore, combineReducers, appMiddleware, compose} from "redux";
+const storeEnhancers = windows.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+
+
+const middleware=[
+
+];
+
+
+const authState = {
+  token: "",
+};
+
+function authReducer(state=authState,action){
+  return state;
+}
+
+const rootReducer=combineReducer({
+  auth: authReducer,
+});
+
+
+const store = createStore(rootReducer,applyMiddleware(...middleware));
