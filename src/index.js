c58db1b3bdcf6a12078415b60cdcabff70cbaea7
Raul Castro <raulc27@gmail.com>
2020-04-29 19:05:06 -0300

    alterando

--- src/index.js
+++ src/index.js
@@ -4,14 +4,16 @@ const storeEnhancers = windows.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 
 const loginSuccess = createAction("LOGIN_SUCCESS");
-const fetchLinksRequest = createAction("FETCH_LINKS_REQUEST");
-const fetchLinksSuccess = createAction("FETCH_LINKS_SUCCESS");
+const loginFailed = createAction("LOGIN_FAILED");
+
+//const fetchLinksRequest = createAction("FETCH_LINKS_REQUEST");
+//const fetchLinksSuccess = createAction("FETCH_LINKS_SUCCESS");
 
 store.dispatch(loginSuccess("aPayload"))
 
 
-const LOGIN_SUCCESS = "LOGIN_SUCCESS";
-const LOGIN_FAILED = "LOGIN_FAILED";
+//const LOGIN_SUCCESS = "LOGIN_SUCCESS";
+//const LOGIN_FAILED = "LOGIN_FAILED";
 
 function loginSuccess(payload){
   return {type:LOGIN_SUCCESS,payload};
@@ -56,7 +58,7 @@ const authState = {
   token: "",
   error:"",
 };
-
+/*
 function authReducer(state=authState,action){
   switch(action.type){
     
@@ -71,6 +73,31 @@ function authReducer(state=authState,action){
       return state;
   }
 }
+*/
+
+const authReducer = createReducer(authState,{
+  [loginSuccess]:(state,action)=>{
+    state.token=action.payload;
+    return state;
+  },
+  [loginFailed]:(state,action)=>{
+    state.error=action.payload;
+    return state;
+  }
+})
+
+
+const authReducer = createReducer(authState, {
+  [loginSuccess]:(state,action)=>{
+
+  },
+
+  [loginFailed]:(state, action)=>{
+    //
+  },
+
+});
+
 
 const rootReducer=combineReducer({
   auth: authReducer,
